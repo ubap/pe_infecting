747d9d5f15e5d04642546281fb1f6768573236cd
jakub <hakujemy@gmail.com>
2016-01-15 15:05:01 +0100

    v3
    dynamic jumping to previous entry point
    param1 is absolute offset value of old and new entry point

--- src/source.cpp
+++ src/source.cpp
@@ -1,12 +1,6 @@
-/*
-	jakub trzebiatowski
-	idea release
-	
-	VS 2012, link with:
-	/SECTION:.text,ERWKP 
-*/
+#include <stdint.h>
 
-#define GET_PARAM_(f) PARAM_ ## f : mov eax, 1337;
+#define GET_PARAM_(f) PARAM_ ## f : mov eax, 1337; 
 
 uint8_t* payLoad;
 uint32_t payLoad_length;
@@ -16,9 +10,22 @@ int32_t SelfCopyingFunction(uint32_t param1 /*access via GET_PARAM_(1)*/)
 	{
 		jmp end;
 start:
-		GET_PARAM_(1);
+		GET_PARAM_(1)		;
+		mov ebx, eax		;
+		call foo
+foo:
+		pop eax				;	get adress of foo
+		mov ecx, foo		;
+		sub ecx, start		;		
+		sub eax, ecx		;	calc addr of start
+		sub eax, ebx		;	calc addr of original entry point
 
-		jmp eax;
+		;;;;;;;;;;;;;
+		;payload here
+
+		;;;;;;;;;;;;;
+		jmp eax				
+
 end:
 		// calc payload length
 		mov eax, end;
@@ -27,9 +34,11 @@ end:
 		mov eax, start;
 		mov payLoad, eax;
 
-		// todo: PRE PROCESSOR LOOP tru params:
+		// PRE PROCESSOR LOOP:
 		mov eax, param1;
 		mov PARAM_1+1, eax;
+
+
 
 	};
 }
